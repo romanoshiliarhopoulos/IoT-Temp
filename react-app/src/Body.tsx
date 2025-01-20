976ce2f8bd6b65fef4fe7bcf5e90d27eff429184
Romanos Chiliarchopoulos <romanoshiliarhopoulos@gmail.com>
2025-01-20 17:01:36 -0600

    finished first version

--- react-app/src/Body.tsx
+++ react-app/src/Body.tsx
@@ -6,13 +6,17 @@ import {
   limit,
   getDocs,
 } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref, onValue } from "firebase/database";
 import { useEffect, useState } from "react";
-import './WeatherScroll.css';
+import "./WeatherScroll.css";
 
+let live: number[] = Array(2).fill(0);
 function Body() {
   // State to store the last entries data
   const [data24, setData24] = useState<any[]>([]);
   const [temp24, setTemp24] = useState<number[]>(Array(24).fill(0));
+  const [time12, setTime12] = useState<string[]>(Array(24).fill(" "));
   const [humidity24, setHumidity24] = useState<number[]>(Array(24).fill(0));
   const [loading, setLoading] = useState(true);
 
@@ -21,6 +25,8 @@ function Body() {
       const data24 = await lastEntries(12 * 6); // Fetch data from Firestore
       console.log(data24);
       console.log(data24[1].humidity);
+
+      //get the current time
 
       let count = 0;
 
@@ -35,15 +41,39 @@ function Body() {
             data24[i + 1].humidity +
             data24[i + 2].humidity) /
           3;
-        temp24[count] = parseFloat(avgtemp.toFixed(2));
-        humidity24[count] = parseFloat(avghum.toFixed(2));
+        temp24[count] = parseFloat(avgtemp.toFixed(1));
+        humidity24[count] = parseFloat(avghum.toFixed(1));
         count++;
       }
+      let timeArray = Array(24).fill(" ");
+      timeArray[0] = "Now";
+      let date = new Date();
+      let stringDate = date.getHours() + ":" + date.getMinutes();
+
+      for (let i = 1; i < timeArray.length; i++) {
+        //populate the time array
+        date.setMinutes(date.getMinutes() - 30);
+        if (date.getMinutes() < 10) {
+          timeArray[i] = date.getHours() + ":0" + date.getMinutes();
+        } else {
+          timeArray[i] = date.getHours() + ":" + date.getMinutes();
+        }
+      }
+      console.log(stringDate);
+      date.setMinutes(date.getMinutes() - 30);
+      //get the live temperature
+      getLiveReadings();
+      temp24[0] = live[0];
+      humidity24[0] = live[1];
+      updateTemperatures(timeArray, temp24);
+      updateHumidities(timeArray, humidity24);
+      console.log(timeArray);
       console.log(temp24);
       console.log(humidity24);
+
+      //populate the time array
       setLoading(false); // Data fetching is complete
     };
-
     fetchData();
   }, []);
 
@@ -98,8 +128,16 @@ function Body() {
         </span>
         <br />
         <br />
-        {temp24[2]}
-        <div className="temperature-scroll" id="tempScroll"></div>
+        <div
+          className="temperature-scroll"
+          id="tempScroll"
+          style={{
+            width: "95%",
+            borderRadius: "10px",
+            alignContent: "center",
+            marginInline: "auto",
+          }}
+        ></div>
       </div>
       <br />
       <div
@@ -142,6 +180,18 @@ function Body() {
           {" "}
           Past 12h
         </span>
+        <br />
+        <br />
+        <div
+          className="humidity-scroll"
+          id="humScroll"
+          style={{
+            width: "95%",
+            borderRadius: "10px",
+            alignContent: "center",
+            marginInline: "auto",
+          }}
+        ></div>
       </div>
     </div>
   );
@@ -183,6 +233,70 @@ async function lastEntries(n: number) {
   } catch (error) {
     console.error("Error getting last entries: ", error);
     return [];
+  }
+}
+
+async function getLiveReadings() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyD38K9ZpLZpFQbGruwO3EnoGSOrhmY45Ug",
+    authDomain: "iot-app-20b70.firebaseapp.com",
+    databaseURL: "https://iot-app-20b70-default-rtdb.firebaseio.com",
+    projectId: "iot-app-20b70",
+    storageBucket: "iot-app-20b70.firebasestorage.app",
+    messagingSenderId: "206130198957",
+    appId: "1x:206130198957:web:a8d92d4c0c923d92004924",
+    measurementId: "G-HQCMWBSZK4",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+  const readingsRef = ref(db, "readings");
+
+  onValue(
+    readingsRef,
+    (snapshot) => {
+      if (snapshot.exists()) {
+        const fetchedData = snapshot.val();
+        live[0] = fetchedData.tempdata;
+        live[1] = fetchedData.humiditydata;
+      }
+    },
+    (error) => {
+      console.error("Error fetching data:", error);
+    }
+  );
+}
+
+function updateTemperatures(time: any[], temp: any) {
+  const container = document.getElementById("tempScroll");
+  container.innerHTML = ""; // Clear existing content
+
+  for (let i = 0; i < temp.length; i++) {
+    const tempItem = document.createElement("div");
+    tempItem.className = "temp-item";
+
+    tempItem.innerHTML = `
+                    <span class="time">${time[i]}</span>
+                    <span class="temp">${temp[i]}°</span>
+                `;
+
+    container.appendChild(tempItem);
+  }
+}
+function updateHumidities(time: any[], hum: any) {
+  const container = document.getElementById("humScroll");
+  container.innerHTML = ""; // Clear existing content
+
+  for (let i = 0; i < hum.length; i++) {
+    const tempItem = document.createElement("div");
+    tempItem.className = "temp-item";
+
+    tempItem.innerHTML = `
+                    <span class="time">${time[i]}</span>
+                    <span class="temp">${hum[i]}%</span>
+                `;
+
+    container.appendChild(tempItem);
   }
 }
 
